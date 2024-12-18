1af46c4029c9567c80805cd04ec00626dd4395ea
iam52 <tupac52@gmail.com>
2024-12-18 17:54:36 +0900

    Initail commit

---
+++ src/main/java/dev/oneboard/domain/OneBoardApplication.java
@@ -0,0 +1,13 @@
+package dev.oneboard.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/oneboard/domain/OneBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.oneboard.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
